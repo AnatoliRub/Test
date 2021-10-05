bfdc5a013041b18e97709fb4e34a40cc61f2d49b
Anatoli Rubankou <catok1992@gmail.com>
2021-10-06 02:37:25 +0300

    jhhjkjhkhkjh

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3002;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+});
   app.enableCors({
     allowedHeaders:"*",
     origin: "*"
