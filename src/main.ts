fc85d09ae6c9783e79e71d863d879fc14250e427
Anatoli Rubankou <catok1992@gmail.com>
2021-10-06 01:53:20 +0300

    dsads

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.PORT;
+  const PORT = process.env.PORT || 3002;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
