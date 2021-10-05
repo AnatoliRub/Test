65aa69e2b043b4d29943ba8ba2cc6b3552f8f21e
Anatoli Rubankou <catok1992@gmail.com>
2021-10-06 02:34:00 +0300

    sadasd

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3002;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors( {origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true});
+  app.enableCors({
+    allowedHeaders:"*",
+    origin: "*"
+  });
   await app.listen(PORT);
 }
 
